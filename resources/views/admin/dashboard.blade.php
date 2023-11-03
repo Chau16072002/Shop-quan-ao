5b616bd6ec13c160934efbab70c3ab9d2f1694e3
Chau16072002 <chau.dmc1607@gmail.com>
2023-11-04 06:24:57 +0700

    Dang-nhap-admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('admin_layout')
+@section('admin_content')
+
+<h1>Chào mừng đến vời Admin</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Fontend
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/trang-chu', [HomeController::class, 'index']);
+
+//Backend
+Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/dashboard', [AdminController::class, 'show_dashboard']);
+Route::post('/admin-dashboard', [AdminController::class, 'dashboard']);
